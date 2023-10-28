755c4e1a2ba7613f1aaaba09b7d20577f773738d
evamariabh <20200783@uthh.edu.mx>
2023-10-27 23:20:38 -0600

    comit3

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -4,7 +4,7 @@
     <title>Bienvenido a Mi Sitio</title>
 </head>
 <body>
-    <h1>Bienvenido a Mi Sitio</h1>
+    <h1>Bienvenido a Mi Sitio!</h1>
     
     <img src="{{ asset('images/laravel.jpg') }}" alt="Ejemplo de Imagen" width="1300" height="500">
 </body>
