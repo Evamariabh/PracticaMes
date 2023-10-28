ed12a31ba1d5945d6e0e1f303c2d7e7661ca1d27
evamariabh <20200783@uthh.edu.mx>
2023-10-27 22:12:19 -0600

    first commit

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenido a Mi Sitio</title>
+</head>
+<body>
+    <h1>Bienvenido a Mi Sitio</h1>
+    
+    <img src="{{ asset('images/laravel.jpg') }}" alt="Ejemplo de Imagen" width="1300" height="500">
+</body>
+</html>
